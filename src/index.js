a5a12544423a5eb17cf897851e6dc2dbeebd9de9
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-14 16:57:33 -0400

    provider added to index

--- src/index.js
+++ src/index.js
@@ -11,4 +11,9 @@ import './styles.scss';
 const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
